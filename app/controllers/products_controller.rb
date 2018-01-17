b17463e1619feba8fd9ed68a83e4f685fe577246
ANDREW SEGAL <segal.a@hotmail.com>
2018-01-17 11:47:36 -0500

    destory function completed

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -21,8 +21,28 @@ class ProductsController < ApplicationController
    end
   end
 
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    @product.update(product_params)
+    if @product.save
+      redirect_to product_path(params[:id])
+    else
+      render :edit
+    end
+  end
+
   def product_params
     params.require(:product).permit(:name, :description, :price_in_cents)
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to products_path
   end
 
 end
