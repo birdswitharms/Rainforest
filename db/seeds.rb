3f0bb788b468415d23ffabf8aa7f324d213c2572
John Carlo Lopez <condottiere.jc@gmail.com>
2018-01-17 14:29:24 -0500

    stretch: DRY up code by adding _product page

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,10 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# Product.create(name: "Banana Holder", description: "It holds bananas!", price_in_cents: 499)
-# Product.create(name: "Banana", description: "It a banana!", price_in_cents: 99)
-# Product.create(name: "Banana Protector", description: "It protects banana while traveling.", price_in_cents: 599)
+Product.create(name: "Banana Holder", description: "It holds bananas!", price_in_cents: 499)
+Product.create(name: "Banana", description: "It a banana!", price_in_cents: 99)
+Product.create(name: "Banana Protector", description: "It protects banana while traveling.", price_in_cents: 599)
+Product.create(name: "Banana candy", description: "10lbs of Banana candy", price_in_cents: 1199)
 
 Review.create(message: "Nice holder", product: Product.find(1))
 Review.create(message: "Great for my headset", product: Product.find(1))
