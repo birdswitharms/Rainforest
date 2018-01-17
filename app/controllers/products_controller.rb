93d9867016f55e6e1aa50e7ca4cc253ad543402c
ANDREW SEGAL <segal.a@hotmail.com>
2018-01-17 11:21:22 -0500

    form created and submit works

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,6 +8,21 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
   end
 
+  def new
+    @product = Product.new()
+  end
 
+  def create
+    @product = Product.new(product_params)
+   if @product.save
+     redirect_to products_path
+   else
+     render :new
+   end
+  end
+
+  def product_params
+    params.require(:product).permit(:name, :description, :price_in_cents)
+  end
 
 end
